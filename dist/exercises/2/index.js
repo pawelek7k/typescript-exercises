86f4421b9557a0c4b7f2f84920392c603ee5870c
pawelek <k.pawelek@vp.pl>
2024-11-17 20:45:59 +0100

    2

--- dist/exercises/2/index.js
+++ dist/exercises/2/index.js
@@ -1,13 +1,33 @@
 "use strict";
 var addressBook = [];
 var addContact = function (addressBook, contact) {
-    addressBook.push(contact);
+    if ([contact.name, contact.email, contact.number].some(function (field) { return field.trim() === ''; })) {
+        console.log('Proszę wypełnić wszystkie dane.');
+    }
+    else {
+        addressBook.push(contact);
+        console.log('Kontakt dodany pomyślnie.');
+    }
 };
 var getContacts = function (addressBook) {
-    addressBook.forEach(function (contact, index) {
-        console.log("".concat(index, ": ").concat(contact.name, ", ").concat(contact.email, ", ").concat(contact.number));
-    });
+    if (addressBook.length === 0) {
+        console.log('Książka adresowa jest pusta.');
+    }
+    else {
+        addressBook.forEach(function (contact, index) {
+            console.log("".concat(index, ": ").concat(contact.name, ", ").concat(contact.email, ", ").concat(contact.number));
+        });
+    }
 };
-addContact(addressBook, { name: 'John Doe', email: 'john.doe@example.com', number: '123-456-790' });
-getContacts(addressBook);
+var getByName = function (addressBook, name) {
+    var foundContacts = addressBook.filter(function (contact) { return contact.name.toLowerCase() === name.toLowerCase(); });
+    if (foundContacts.length > 0) {
+        foundContacts.forEach(function (contact, index) {
+            console.log("Znaleziono kontakt: ".concat(index, ": ").concat(contact.name, ", ").concat(contact.email, ", ").concat(contact.number));
+        });
+    }
+    else {
+        console.log("Nie znaleziono kontaktu o imieniu: ".concat(name));
+    }
+};
 //# sourceMappingURL=index.js.map
