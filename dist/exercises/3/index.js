f75ea90ab6968743c99a4a16ed63ef606ca68d2d
pawelek <k.pawelek@vp.pl>
2024-11-18 21:06:59 +0100

    3

--- dist/exercises/3/index.js
+++ dist/exercises/3/index.js
@@ -3,8 +3,17 @@ var registerUser = function (user) {
     if ("adminCode" in user) {
         console.log('Admin created');
     }
+    else if ("guestCode" in user) {
+        console.log('Guest created');
+    }
     else {
         console.log('User created');
     }
 };
+var user = { username: "johndoe", email: "johndoe@example.com" };
+var admin = { username: "admin1", email: "admin1@example.com", adminCode: "1234" };
+var guest = { username: "guest1", email: "guest1@example.com", guestCode: "5678" };
+registerUser(user);
+registerUser(admin);
+registerUser(guest);
 //# sourceMappingURL=index.js.map
--- exercises/3/index.ts
+++ exercises/3/index.ts
@@ -7,10 +7,24 @@ interface Admin extends User {
     adminCode: string;
 }
 
-const registerUser = (user: User | Admin) => {
+interface Guest extends User {
+    guestCode: string
+}
+
+const registerUser = (user: User | Admin | Guest) => {
     if ("adminCode" in user) {
         console.log('Admin created')
+    } else if ("guestCode" in user) {
+        console.log('Guest created')
     } else {
         console.log('User created')
     }
 }
+
+const user: User = { username: "johndoe", email: "johndoe@example.com" };
+const admin: Admin = { username: "admin1", email: "admin1@example.com", adminCode: "1234" };
+const guest: Guest = { username: "guest1", email: "guest1@example.com", guestCode: "5678" };
+
+registerUser(user);
+registerUser(admin);
+registerUser(guest);
